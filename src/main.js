4596de8273f1f84c3de5f2dd750fdd72a4db0061
ForEars <1228456740@qq.com>
2022-06-27 21:20:20 +0800

    打包时生成html文件, 并且html文件自动引入打包后的js文件

--- src/main.js
+++ src/main.js
@@ -2,3 +2,6 @@ import './banner.js'
 import './tabs.js'
 
 import './style/index.css'
+import './style/index.less'
+import $ from 'jquery'
+// $('#swiper').css('background-color', 'red')
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -11,6 +12,17 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
     ],
   },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
 }
